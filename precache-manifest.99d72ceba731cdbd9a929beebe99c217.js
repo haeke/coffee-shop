4e1ee4e3c9a17e0149e64dbf833d403a1c83fb0d
haeke <riverae2@student.wpunj.edu>
2019-02-08 16:13:27 -0500

    Updates

---
+++ precache-manifest.99d72ceba731cdbd9a929beebe99c217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef05d966da878df3bbf3cb897cd90f84",
+    "url": "/coffee-shop/static/media/Double_Espresso.ef05d966.mp4"
+  },
+  {
+    "revision": "f4ad6227f2e8e2e232ce",
+    "url": "/coffee-shop/static/js/runtime~main.f4ad6227.js"
+  },
+  {
+    "revision": "4a90b1e039b3baffd815",
+    "url": "/coffee-shop/static/js/main.4a90b1e0.chunk.js"
+  },
+  {
+    "revision": "4abc30e5cb74af00cb67",
+    "url": "/coffee-shop/static/js/1.4abc30e5.chunk.js"
+  },
+  {
+    "revision": "4a90b1e039b3baffd815",
+    "url": "/coffee-shop/static/css/main.82ea7b3b.chunk.css"
+  },
+  {
+    "revision": "9de12ef2c41a6aa3bd9ed2f3c25ea788",
+    "url": "/coffee-shop/index.html"
+  }
+];
