88f9e2c7e8ae255c29ccd5e4ece55fb3bfb4b07f
haeke <riverae2@student.wpunj.edu>
2019-02-15 15:29:13 -0500

    added a note to the api function

--- src/api/restaurant.js
+++ src/api/restaurant.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 let baseURL = "http://localhost:3040";
+// When pushing to heroku we need to use the heroku domain to make API requests
 if (process.env.NODE_ENV === "production") {
   baseURL = "https://the-espresso-shop.herokuapp.com/";
 }
