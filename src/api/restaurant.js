ec7ed218298d7efdb532f490116531c09570d99a
haeke <riverae2@student.wpunj.edu>
2019-02-15 15:22:30 -0500

    changes base url that the API uses

--- src/api/restaurant.js
+++ src/api/restaurant.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+let baseURL = "http://localhost:3040";
+if (process.env.NODE_ENV === "production") {
+  baseURL = "https://the-espresso-shop.herokuapp.com/";
+}
 export default axios.create({
-  baseURL: "http://localhost:3040"
+  baseURL: baseURL
 });
