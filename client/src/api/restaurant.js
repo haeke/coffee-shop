73c2fbdc44f826848f45fce3f196eebba138c323
haeke <riverae2@student.wpunj.edu>
2019-02-15 15:47:34 -0500

    changed baseurl used in production

--- client/src/api/restaurant.js
+++ client/src/api/restaurant.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let baseURL = "http://localhost:3040";
 // When pushing to heroku we need to use the heroku domain to make API requests
 if (process.env.NODE_ENV === "production") {
-  baseURL = "https://the-espresso-shop.herokuapp.com/";
+  baseURL = "https://the-espresso-shop.herokuapp.com";
 }
 export default axios.create({
   baseURL: baseURL
