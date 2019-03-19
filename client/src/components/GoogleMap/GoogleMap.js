d570381ffaaef05b6593797a2ba46f33d85711a3
haeke <riverae2@student.wpunj.edu>
2019-03-19 12:54:25 -0400

    added a marker and info window to the GoogleMap component.

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 
 const mapStyles = {
   width: "100%",
@@ -7,7 +7,14 @@ const mapStyles = {
 };
 
 class GoogleMap extends Component {
+  state = {
+    showInfoWindow: false
+  };
+
+  onMarkerClick = () => {};
   render() {
+    console.log(process.env.REACT_APP_GOOGLE_API_KEY);
+    const { showInfoWindow } = this.state;
     return (
       <div style={{ position: "relative", width: "100%", height: "100%" }}>
         <Map
@@ -18,12 +25,21 @@ class GoogleMap extends Component {
             lat: -1.2285,
             lng: 36.8233
           }}
-        />
+        >
+          <Marker
+            name={"Your position"}
+            position={{ lat: -1.2285, lng: 36.8233 }}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={showInfoWindow}
+          />
+        </Map>
       </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY
 })(GoogleMap);
